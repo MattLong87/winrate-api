d9b3c2a2b1b00342e829f7d3fdf89fc32a4dbb4b
Matthew Long <MattLong87@gmail.com>
2017-06-08 16:12:50 -0400

    set up functions to populate test database

--- test/test-server.js
+++ test/test-server.js
@@ -4,6 +4,7 @@ const { TEST_DATABASE_URL } = require('../config');
 const { app, runServer, closeServer } = require('../server');
 const mongoose = require('mongoose');
 const faker = require('faker');
+const { User } = require('../models/models');
 
 const should = chai.should();
 chai.use(chaiHttp);
@@ -15,12 +16,45 @@ function seedUsers(numUsers) {
   for (let i = 0; i < numUsers; i++) {
     let user = [faker.internet.email(), faker.internet.password()]
     fakeUsers.push(user);
-    return User.hashPassword(password)
+    return User.hashPassword(user[1])
       .then(function (hash) {
-        return User.create(generateFakeUser(email, hash))
+        return User.create(generateFakeUser(user[0], hash))
           .catch((err) => console.log(err))
+          //.then((user) => console.log(user))
       })
   }
+}
+
+function generateFakeUser(email, hash) {
+  //5 random first names to use as players
+  let players = [faker.name.firstName(), faker.name.firstName(), faker.name.firstName(), faker.name.firstName(), faker.name.firstName()];
+  //5 random game names
+  let games = [faker.commerce.productName(), faker.commerce.productName(), faker.commerce.productName(), faker.commerce.productName(), faker.commerce.productName()]
+  let sessions = generateFakeSessions(Math.floor(Math.random()*10), players, games);
+  return {
+    password: hash,
+    email: email,
+    created: Date.now(),
+    name: {
+      firstName: faker.name.firstName(),
+      lastName: faker.name.lastName()
+    },
+    sessions: sessions
+  }
+}
+
+function generateFakeSessions(numSessions, players, games){
+  let sessions = [];
+  for (let i=0; i<numSessions; i++){
+    let session = {
+      game: games[Math.floor(Math.random()*(games.length - 1))],
+      players: players,
+      winner: players[Math.floor(Math.random()*(players.length - 1))],
+      date: Date.now()
+    };
+    sessions.push(session);
+  }
+  return sessions;
 }
 
 function tearDownDb() {
@@ -51,33 +85,33 @@ describe('Winrate API', function () {
       });
   });
 
-  describe('User login endpoint', function(){
-    it('should log in a user on a POST request', function(){
+  describe('User login endpoint', function () {
+    it('should log in a user on a POST request', function () {
 
     })
   })
 
-  describe('Create user endpoint', function(){
-    it('should create new user on a POST request', function(){
+  describe('Create user endpoint', function () {
+    it('should create new user on a POST request', function () {
 
     })
   })
 
-  describe('/users/me endpoint', function(){
-    it('should return user information on a GET request', function(){
+  describe('/users/me endpoint', function () {
+    it('should return user information on a GET request', function () {
 
     })
   })
 
-  describe('/users/me/add-session endpoint', function(){
-    it('should add a session to user on a POST request', function(){
+  describe('/users/me/add-session endpoint', function () {
+    it('should add a session to user on a POST request', function () {
 
     })
   })
 
-  describe('/users/me/sessions endpoint', function(){
-    it('should delete a session on a DELETE request', function(){
-      
+  describe('/users/me/sessions endpoint', function () {
+    it('should delete a session on a DELETE request', function () {
+
     })
   })
 });
